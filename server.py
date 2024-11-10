926157228b07c395d0eb2933d0fc6b5d938de992
Max <maximilian.bolt@student.manchester.ac.uk>
2024-11-10 06:37:44 +0000

    more ai text

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def authenticateSpotify():
     parameters = {
         "response_type": 'code',
         "client_id": client_id,
-        "scope": 'user-read-private user-read-email',
+        "scope": 'user-top-read playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-private user-read-email',
         "redirect_uri": "http://localhost:3000/spotify/accept_oauth_link",
         "state": "1111111111111111"#''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
     }
@@ -44,7 +44,7 @@ def getTop50(token):
 
     #track_names = [item["name"] for item in tracksList["items"]]
 
-    return str(tracksList)
+    return tracksList
 
 
 @app.route('/get_user_profile/<token>', methods=['GET'])
@@ -55,7 +55,7 @@ def getProfile(token):
     }   
     response = requests.get(url, headers=headers)
     response = json.loads(response.content.decode('utf-8'))
-    return str(response)
+    return response
 
 
 
@@ -76,12 +76,33 @@ def getAccessToken():
     response = requests.post(url = "https://accounts.spotify.com/api/token", data = payload, headers=headers)
 
     response = json.loads(response.content.decode('utf-8'))
-    return str(response)
+    return response
 
-@app.route('/getAI/<inputList>', methods=['GET'])
-def getAIResponse(inputList):
-        
-    prompt = "Put your DEFAULT prompt here" + str(inputList)
+@app.route('/getAI/<input>', methods=['GET'])
+def getAIResponse(input):
+
+    #TMP INPUT
+    input = {
+        "time frame 1": [["Song1","Song2", "Song3", "Song4"], ["Song4","Song5", "Song6"],["Song4","Song5","Song1"]],
+        "time frame 2": [["Song1","Song2", "Song3", "Song4"], ["Song4","Song1", "Song10"],["Song4","Song3","Song1"]],
+        "time frame 3": [["Song1","Song2", "Song3", "Song4"], ["Song4","Song3", "Song10"],["Song4","Song3","Song10"]]
+    }
+
+    # assume input of form: {time frame 1:[UserList1,Userlist2....UserListN], time frame 2:[...], time frame 3:[...]}
+    #time frame 1 is shortest (4 weeks)
+    timeframe1 = input['time frame 1'] #4 weeks
+    timeframe2 = input['time frame 2'] #6 months
+    timeframe3 = input['time frame 3'] #1 year
+
+    History = "Over the last 4 weeks the group has listened to {timeframe1}. "\
+        "Over the last 6 months the group has listened to {timeframe2}. "\
+        "Over the last year the group has listened to {timeframe3}. "
+    
+    prompt = "You are an analyst whos job is to summarize how a group of music peoples taste overlaps over a period of time. "\
+        "You should focus on the common ground as well as differences between people, espeically how it varies as time passes. "\
+        "Use the historic listening data made available to you: {History}"
+    
+
     output = aiResponse.generateResponse(prompt)
     return output
 
