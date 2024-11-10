426e8716a61e4a772c48cf8964f8df285025fd0a
aap9002 <aap9002@gmail.com>
2024-11-10 02:05:47 +0000

    fix header

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -5,7 +5,7 @@ import LOGO from '@/public/logo.png'
 
 const Header = () => {
   return (
-    <div className='bg-[var(--accent)] p-4 text-black border-b-4 shadow-md flex justify-center text-white' >
+    <div className='bg-[var(--accent)] p-4 text-black border-b-4 border-gray-300 shadow-md flex justify-center text-white' >
       <div className='flex justify-between w-full items-center max-w-[1000px] flex-wrap'>
         <Image src={LOGO} alt='logo' width={200} height={70} className='rounded-md'/> 
         <div className=' flex w-[400px] justify-between p-3'>
